c7bedecfa6f9fa394874d261b5f3bdc9725de462
david-aihe <doa9200@gmail.com>
2022-03-01 15:11:01 -0500

    Authorization setup complete

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,52 @@
+const User = require('./auth-model')
+
+const checkUsernameFree = async (req, res, next) => {
+    try {
+      const users = await User.findBy({username: req.body.username})
+      if(!users.length){
+        next()
+      }
+      else{
+        next({status: 400, message: "username taken"})
+      }
+    } 
+    catch (error) {
+      next(error)
+    }
+}
+
+const validatePayload = (req, res, next) => {
+    const { username, password } = req.body
+
+    if(!username || !password){
+        next({
+            status: 401,
+            message: "username and password required"
+        })
+    }
+    else{
+        next()
+    }
+}
+
+const checkUsernameExists = async (req, res, next) => {
+    try {
+      const [user] = await User.findBy({username: req.body.username})
+      if(!user){
+        next({status:401, message: "invalid credentials"})
+      }
+      else{
+        req.user = user
+        next()
+      }
+    } 
+    catch (error) {
+      next(error)
+    }    
+  }
+
+module.exports = {
+    checkUsernameFree,
+    validatePayload,
+    checkUsernameExists
+}
---
+++ api/auth/auth-router.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const tokenBuilder = require('./generateToken')
+const User = require('./auth-model')
+const {checkUsernameFree, validatePayload, checkUsernameExists} = require('./auth-middleware')
+
+router.post('/register', validatePayload, checkUsernameFree, (req, res, next) => {
+  const {username, password} = req.body
+
+  const hash = bcrypt.hashSync(password, 8)
+  
+  User.add({username, password: hash})
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err=>{
+      next(err)
+    })
+});
+
+router.post('/login', validatePayload, checkUsernameExists, (req, res, next) => {
+  if(bcrypt.compareSync(req.body.password, req.user.password)){
+    const token = tokenBuilder(req.user)
+    res.json({
+      status: 200,
+      message: `welcome, ${req.user.username}`,
+      token,
+    })
+  }
+  else{
+    next({
+      status:401, 
+      message: 'invalid credentials'
+    })
+  }
+});
+
+module.exports = router;
