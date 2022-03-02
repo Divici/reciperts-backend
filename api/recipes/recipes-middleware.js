ec8b4cd12f21c7163d60bd847e8b963ba449c8e5
david-aihe <doa9200@gmail.com>
2022-03-02 15:35:41 -0500

    recipes middleware finished

--- api/recipes/recipes-middleware.js
+++ api/recipes/recipes-middleware.js
@@ -36,25 +36,21 @@ exports.checkRecipeId = async (req, res, next) => {
 
 exports.checkRecipePayload = (req, res, next) => {
     const error = {status: 400}
-    // const {name, budget} = req.body;
-    // if(name === undefined || budget === undefined){
-    //   error.message ="name and budget are required"
-    // }
-    // else if(name.trim().length < 3 || name.trim().length > 100){
-    //   error.message = "name of account must be between 3 and 100"
-    // }
-    // else if(typeof budget !== 'number' || isNaN(budget)){
-    //   error.message = "budget of account must be a number"
-    // }
-    // else if(budget < 0 || budget > 1000000){
-    //   error.message = "budget of account is too large or too small"
-    // }
     
-    // if(error.message){
-    //   next(error)
-    // }
-    // else{
-    //   req.name = name.trim()
-    //   next()
-    // }
+    const {recipe_name, ingredient_name, quantity, step_number, step_instruction} = req.body;
+    if(!step_instruction || !step_number){
+      error.message ="step instructions and numbers are required"
+    }
+    else if(!quantity){
+      error.message ="Ingredient quantity is needed"
+    }
+
+    if(error.message){
+      next(error)
+    }
+    else{
+      req.recipe_name = recipe_name.trim()
+      req.ingredient_name = ingredient_name.trim()
+      next()
+    }
   }
