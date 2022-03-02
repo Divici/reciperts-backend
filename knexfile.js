7cd45bf9324c4b2decabb4119e5190039c68daf5
david-aihe <doa9200@gmail.com>
2022-03-02 17:03:05 -0500

    deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,29 @@
+require('dotenv').config()
+
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
 const sharedConfig = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-  }
-  
-  module.exports = {
-    development: {
-      ...sharedConfig,
-      connection: { filename: './data/cookbook.db3' },
-      seeds: { directory: './data/seeds' },
-    },
-    testing: {
-      ...sharedConfig,
-      connection: { filename: './data/test.db3' },
-    },
-  };
-  
+  client: 'pg',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
+  },
+  testing: {
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL,
+  },
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  },
+}
