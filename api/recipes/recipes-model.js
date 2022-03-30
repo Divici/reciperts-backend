c7a82b9313909c1e8dfe0b4fbdf0571d3d01755f
david-aihe <doa9200@gmail.com>
2022-03-30 10:34:27 -0400

    edit recipes model, getById

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -9,52 +9,61 @@ async function findById(recipe_id) {
 }
 
 const getById = async (recipe_id) => {
-    const recipeRows = await db('recipes as r')
+    const recipe = await db('recipes')
+        .where('recipe_id', recipe_id)
+
+    const stepsRows = await db('recipes as r')
         .join('steps as st', 'r.recipe_id', 'st.recipe_id')
         .where('r.recipe_id', recipe_id)
-        .select("r.*", "st.*")
+        .select("st.*")
         .orderBy('st.step_number')
 
     const ingredientsRows = await db('recipes as r')
         .join("ingredients as ing", "r.recipe_id", "ing.recipe_id")
         .where('r.recipe_id', recipe_id)
-        .select("r.*", "ing.*")
+        .select( "ing.*")
         .orderBy('ing.ingredient_id')
 
+
     const result = {
-        recipe_id: recipeRows[0].recipe_id,
-        recipe_name: recipeRows[0].recipe_name,
-        category: recipeRows[0].category,
-        source: recipeRows[0].source,
-        prep_time: recipeRows[0].prep_time,
-        cook_time: recipeRows[0].cook_time,
-        ingredients: [],
-        steps: []
+        recipe: recipe,
+        steps: stepsRows,
+        ingredients: ingredientsRows
     }
+    // const result = {
+    //     recipe_id: recipeRows[0].recipe_id,
+    //     recipe_name: recipeRows[0].recipe_name,
+    //     category: recipeRows[0].category,
+    //     source: recipeRows[0].source,
+    //     prep_time: recipeRows[0].prep_time,
+    //     cook_time: recipeRows[0].cook_time,
+    //     ingredients: [],
+    //     steps: []
+    // }
 
-    recipeRows.forEach(step=>{
-        if(step.step_number){
-            result.steps.push({
-                step_id: step.step_id,
-                step_number: step.step_number,
-                step_instruction: step.step_instruction
-            })
-        }
-    })
+    // recipeRows.forEach(step=>{
+    //     if(step.step_number){
+    //         result.steps.push({
+    //             step_id: step.step_id,
+    //             step_number: step.step_number,
+    //             step_instruction: step.step_instruction
+    //         })
+    //     }
+    // })
 
-    if (recipeRows[0].ingredient_id === null) {
-        return result;
-    }
+    // if (recipeRows[0].ingredient_id === null) {
+    //     return result;
+    // }
 
-    for (let ingredient of ingredientsRows) {
-        result.ingredients.push({
-            ingredient_id: ingredient.ingredient_id,
-            ingredient_name: ingredient.ingredient_name,
-            ingredient_unit: ingredient.ingredient_unit,
-            quantity: ingredient.quantity
-        });
-    }
-    console.log(typeof(Number(recipe_id)));
+    // for (let ingredient of ingredientsRows) {
+    //     result.ingredients.push({
+    //         ingredient_id: ingredient.ingredient_id,
+    //         ingredient_name: ingredient.ingredient_name,
+    //         ingredient_unit: ingredient.ingredient_unit,
+    //         quantity: ingredient.quantity
+    //     });
+    // }
+    // console.log(typeof(Number(recipe_id)));
     return result
 }
 
