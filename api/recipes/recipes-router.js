bacea8404fa0a5d7606bb1871de8753076faf0eb
david-aihe <doa9200@gmail.com>
2022-04-01 21:35:39 -0400

    create fix

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,10 +4,20 @@ const restrict = require('../middleware/restricted')
 const {checkRecipeNameUnique, checkRecipeId, checkRecipePayload} = require('./recipes-middleware')
 
 router.get('/', (req, res, next) => {
-    res.json({message: "Welcome. Please sign in"})
+    //res.json({message: "Welcome. Please sign in"})
+    try {
+        const recipes = await RecipesModel.getAll()
+        res.status(200).json(recipes)
+      } 
+    catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'An error occurred while trying to retrieve recipes'
+        })
+      }
   })
   
-router.get('/:recipe_id', restrict, async (req, res, next) => {
+router.get('/:recipe_id', async (req, res, next) => {
     try {
         const recipe = await RecipesModel.getById(req.params.recipe_id)
         if (recipe) {
@@ -23,7 +33,7 @@ router.get('/:recipe_id', restrict, async (req, res, next) => {
     }
 })
 
-router.post('/', restrict, checkRecipePayload, checkRecipeNameUnique, async (req, res, next) => {
+router.post('/', checkRecipePayload, checkRecipeNameUnique, async (req, res, next) => {
     try {
         const newRecipe = await RecipesModel.create(req.body)
         res.status(201).json(newRecipe);
